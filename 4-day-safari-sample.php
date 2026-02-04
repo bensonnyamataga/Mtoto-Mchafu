02c53103a02df5688ec031c6ddec0aa8f4db077f
bensonnyamataga <bensonnyamataga@gmail.com>
2026-02-04 12:57:23 +0300

    mid-range

--- 4-day-safari-sample.php
+++ 4-day-safari-sample.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4 Days Mid-Range Safari - Tarangire, Serengeti & Ngorongoro Crater title>
+    <title>4 Days Mid-Range Safari - Tarangire, Serengeti & Ngorongoro <title>
     <meta name="description" content="Experience a 4-day mid-range safari adventure in Tanzania's iconic Tarangire, Serengeti, and Ngorongoro Crater.">
     <meta name="keywords" content="4 days Tanzania safari, mid-range safari, Tarangire, Serengeti, Ngorongoro Crater, safari package, balloon safari">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
