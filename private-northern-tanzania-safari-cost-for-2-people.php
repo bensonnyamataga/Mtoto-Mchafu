b8118e3743a450772a8bd22952069b57178c1189
bensonnyamataga <bensonnyamataga@gmail.com>
2026-03-08 11:57:16 +0300

    Update title for Private Northern Tanzania Safari Cost page to improve clarity

--- private-northern-tanzania-safari-cost-for-2-people.php
+++ private-northern-tanzania-safari-cost-for-2-people.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Private  Northern Tanzania Safari Cost for 2 People Couples Pricing</title>
+    <title>Private Northern Tanzania Safari Cost for 2 People </title>
     <meta name="description" content="Exact private Northern Tanzania safari cost for 2 people. 3-7 day private safaris for couples. Mid-range to luxury, all park fees included + free quote.">
     <meta name="keywords" content="private Northern Tanzania safari cost for 2 people, Tanzania safari for couples, private safari for two, Northern Circuit private tour price">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
